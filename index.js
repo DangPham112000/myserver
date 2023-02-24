f6a5f05d72bde5e1fc77fce01eafdf5a4b786d6e
DangPham112000 <dangpham112000@gmail.com>
2023-02-24 15:55:51 +0700

    change method

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.set("trust proxy", true);
 
-app.get("/", (req, res) => {
-    console.dir(req.ip);
+app.get("/ip/all", (req, res) => {
     var ip = {
         ip: req.ip,
         headers: req.headers["x-forwarded-for"],
         socket: req.socket.remoteAddress,
     };
     res.send(JSON.stringify(ip));
+});
+
+app.get("/", (req, res) => {
+    res.send(
+        req.ip || req.headers["x-forwarded-for"] || req.socket.remoteAddress
+    );
 });
 
 app.listen(80, (err) => {
